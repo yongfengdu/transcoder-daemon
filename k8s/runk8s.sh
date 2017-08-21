2589e4c2cb3c49320778da6efbc83a131030eb93
Yongfeng Du <dolpher.du@intel.com>
2017-08-21 12:59:34 +0800

    Update the image location
    Change-Id: If0178472891b33079999475cef7b672c00c7807a

--- k8s/runk8s.sh
+++ k8s/runk8s.sh
@@ -5,13 +5,11 @@ sudo kubectl version
 if [ $? != 0 ]; then
     ./installk8s.sh
 fi
-# Pull the transcode image, https://github.com/LinEricYang/transcoder-env
-sudo docker pull fullterrors/transcoder-daemon
+# Original dockerfile from  https://github.com/LinEricYang/transcoder-env
+sudo docker pull dolpher/transcoder-daemon
 
-sudo docker tag fullterrors/transcoder-daemon transcoder-daemon
+sudo docker tag dolpher/transcoder-daemon transcoder-daemon
 
-# get the latest pod definition from here
-# wget https://raw.githubusercontent.com/LinEricYang/transcoder-env/master/k8s/transcoder-pod.yml
 sudo mkdir /opt/media
 sudo chown $USER /opt/media
 
