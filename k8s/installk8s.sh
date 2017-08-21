8ec560544233c3c92bc22f6399f8397c6cf0adad
Yongfeng Du <dolpher.du@intel.com>
2017-08-21 13:12:48 +0800

    simple fix
    Change-Id: I4ee519e3e36cc5113a16edbaea0167c4c872914f

--- k8s/installk8s.sh
+++ k8s/installk8s.sh
@@ -24,7 +24,7 @@ ins_kubeadm
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
